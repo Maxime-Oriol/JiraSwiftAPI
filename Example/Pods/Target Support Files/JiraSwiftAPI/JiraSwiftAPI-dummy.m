2c6c81dd891544503bafd72bc9ab1b33cfd40c35
Maxime ORIOL <moriol@tf1.fr>
2020-10-09 13:30:56 +0200

    Init

---
+++ Example/Pods/Target Support Files/JiraSwiftAPI/JiraSwiftAPI-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_JiraSwiftAPI : NSObject
+@end
+@implementation PodsDummy_JiraSwiftAPI
+@end
---
+++ Example/Pods/Target Support Files/Pods-JiraSwiftAPI_Tests/Pods-JiraSwiftAPI_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JiraSwiftAPI_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_JiraSwiftAPI_Tests
+@end
