6e15fe25fb1f8951892d59f7211212adde808c21
Maxime ORIOL <moriol@tf1.fr>
2020-10-12 08:42:05 +0200

    Ignore configuration file for security reason

---
+++ Example/JiraSwiftApiExample/Configuration.swift
@@ -0,0 +1,8 @@
+//
+//  Configuration.swift
+//  JiraSwiftApiExample
+//
+//  Created by ORIOL.OMNILOG, MAXIME on 12/10/2020.
+//
+
+import Foundation
--- Example/JiraSwiftApiExample/RequestManager.swift
+++ Example/JiraSwiftApiExample/RequestManager.swift
@@ -13,36 +13,38 @@ class RequestManager {
     private var previousSprint: JiraSprint?
     
     init() {
-        
+        // You have to create your own "Configuration" struct with credentials
         // Go there to create your token ; works only for cloud hosted projects :
         // https://id.atlassian.com/manage-profile/security/api-tokens
-        let config = (
-            server: "https://myAmazingProject.atlassian.net",
-            username: "myPersonalLogin",
-            token: "generatedPrivateToken",
-            boardId: 1
-        )
         
-        self.jiraManager = try! JiraAPI(server: config.server, username: config.username, password: config.token, boardId: config.boardId)
+        
+        self.jiraManager = try! JiraAPI(server: Configuration.server,
+                                        username: Configuration.username,
+                                        password: Configuration.token,
+                                        boardId: Configuration.boardId)
+        
         self.jiraManager.delegate = self
         self.jiraManager.enableCache(enabled: false)
     }
     
     func callAPI() {
-        self.jiraManager.getConfiguration()
+        self.jiraManager.getAllVersions()
     }
 
 }
 
 
-extension RequestManager: JiraAPIDelegate {
+extension RequestManager: JiraAPIDelegate {    
     
-    func didGetConfiguration(configuration: JiraConfiguration?) {
-        guard let configuration = configuration else {
-            print("There is no available configuration")
+    func didGetAllVersions(versions: JiraVersions?) {
+        guard let versions = versions else {
+            print("There is no available versions")
             return
         }
         
-        print("Configuration name: "+configuration.name)
+        print("Versions are:")
+        for version in versions.values {
+            print(version.name)
+        }
     }
 }
